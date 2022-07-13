81b706f25af01e561166b280868d55ed401cf8c9
jteaaa <clkong@usc.edu>
2022-07-12 23:52:30 -0700

    menu + level select

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    
+    [SerializeField] int selectedLevel;
+
+    // public AudioSource completeLevelAudio;
+
+    public void LoadSelectedLevel()
+    {
+        SceneManager.LoadScene(selectedLevel);
+
+    }
+
+    // IEnumerator OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //             completeLevelAudio.Play();
+
+    //             yield return new WaitForSeconds(completeLevelAudio.clip.length);
+    //             Debug.Log(completeLevelAudio.clip.length);
+    //             if (gameObject.tag == "Levels") {
+    //                 SceneManager.LoadScene("LevelSelect");
+    //             }
+    //             else {
+    //                 SceneManager.LoadScene(selectedLevel);
+    //             }
+    //       }
+    //  }
+}
